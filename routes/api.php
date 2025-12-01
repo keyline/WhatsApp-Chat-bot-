bd2063ac6e0093e9c3da481d95024bce724bc6ec
Somnath Shil <somnath.shil@keylines.net>
2025-12-01 18:51:50 +0530

    New controller added to check db query for finding user working or not

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,6 @@
 <?php
-// use Illuminate\Support\Facades\Route;
-// use App\Http\Controllers\BotWebhookController;
-
-// Route::match(['get', 'post'], '/webhook/bot/{token}', [BotWebhookController::class, 'handle'])
-//     ->name('bot.webhook');
 
 use Illuminate\Support\Facades\Route;
-// use App\Http\Controllers\BotWebhookController;
 use App\Http\Controllers\BotWebhookControllerTwo;
 
 Route::match(['get', 'post'], '/webhook/bot', [BotWebhookControllerTwo::class, 'handle']);
